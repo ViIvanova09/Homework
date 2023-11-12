1ec1236ed2b2ee842a7b6b338b0a695679d8e40a
ViIvanova09 <129741412+ViIvanova09@users.noreply.github.com>
2023-11-12 13:50:24 +0200

    asd test

---
+++ Homework JS advanced/filterWordsStartingWithLetter.js
@@ -0,0 +1,6 @@
+let quote = "A common mistake that people make when trying to design something completely foolproof is to underestimate the ingenuity of complete fools.";
+
+function filterText(text){
+const words = 
+
+}
